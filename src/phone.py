0baa433032a018e472040ef2100e9577882b4307
panthurr0 <olapopaola@mail.ru>
2024-01-25 23:53:00 +0300

    editing

--- src/phone.py
+++ src/phone.py
@@ -3,10 +3,21 @@ from src.item import Item
 
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        """
+        Создание унаследованного из класса Item класса Phone
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param number_of_sim: Количество поддерживаемых сим-карт
+        """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
     def __repr__(self):
+        """
+        :return: 'Item'("Название", "Цена", "Количество", "Количество поддерживаемых Sim-карт")
+        """
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     @property
